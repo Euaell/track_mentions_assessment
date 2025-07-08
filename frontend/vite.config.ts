4d3255bc002d6fbd441c5c0bc15f069c328c83fc
Euael <euaelmeko@gmail.com>
2025-07-08 15:26:15 +0300

    chore: fix the vite config for github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(() => {
+  const isGhPages = process.env.GITHUB_PAGES === 'true'
+  return {
+    plugins: [react()],
+    base: isGhPages ? '/track_mentions_assessment/' : '/',
+  }
 })
